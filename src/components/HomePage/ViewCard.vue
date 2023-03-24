8625d508513246a32c941ea03272cf38e56e3e58
Ruslan Shevcov <sasa02sasa@mail.ru>
2023-03-25 01:37:36 +0600

    rename home page component and add new component "ViewCard" to basic template in home page

---
+++ src/components/HomePage/ViewCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper-card col-6">
+    <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">{{ title }}</div>
+        <div class="text-subtitle2">{{ description }}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-btn outline color="white" label="Перейти" :to="link" />
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ViewCard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+});
+</script>
+
+<style></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page class="flex column q-pa-lg">
+    <h4>Добро пожаловать!</h4>
+    <div class="row q-col-gutter-md">
+      <ViewCard
+        v-for="card in cards"
+        :title="card.title"
+        :description="card.description"
+        :key="card.id"
+        :link="card.link"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import ViewCard from "src/components/HomePage/ViewCard.vue";
+export default defineComponent({
+  name: "HomePage",
+  components: {
+    ViewCard,
+  },
+  setup() {
+    const cards = [
+      {
+        title: "Главная страница",
+        description: "Информация о главной странице",
+        link: "/",
+        id: 1,
+      },
+      {
+        title: "Новости",
+        description: "Просмотр новостей",
+        link: "/articles",
+        id: 2,
+      },
+      {
+        title: "Профиль",
+        description: "Ваш профиль",
+        link: "/profile",
+        id: 3,
+      },
+      {
+        title: "FAQ",
+        description: "Ответы на частозадаваемые вопросы",
+        link: "/faq",
+        id: 4,
+      },
+    ];
+    return { cards };
+  },
+});
+</script>
+<style lang="scss"></style>
