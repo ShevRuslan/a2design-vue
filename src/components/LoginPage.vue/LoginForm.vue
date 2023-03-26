85a41840f229163298a8d19b11f39a4a4765ce62
Ruslan Shevcov <sasa02sasa@mail.ru>
2023-03-26 14:35:05 +0600

    add login page and login to auth user

---
+++ src/components/LoginPage.vue/LoginForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-form @submit.prevent="send" class="q-pa-xs q-gutter-y-md">
+    <q-input
+      outlined
+      v-model="username"
+      label="Ваш логин"
+      lazy-rules
+      :rules="[(val) => (val && val.length > 0) || 'Обязательное поле!']"
+    />
+    <q-input
+      type="password"
+      outlined
+      v-model="password"
+      label="Ваш пароль"
+      lazy-rules
+      :rules="[(val) => (val && val.length > 0) || 'Обязательное поле!']"
+    />
+    <q-btn
+      label="Отправить"
+      type="submit"
+      color="primary"
+      class="col-6 full-width"
+      :loading="submitting"
+    >
+      <template v-slot:loading> <q-spinner-facebook /> </template>
+    </q-btn>
+  </q-form>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+export default defineComponent({
+  name: "LoginForm",
+  setup() {
+    const router = useRouter();
+
+    const $q = useQuasar();
+
+    const username = ref("");
+    const password = ref("");
+    const submitting = ref(false);
+
+    const send = () => {
+      if (
+        username.value.trim() === "User" &&
+        password.value.trim() === "12345"
+      ) {
+        submitting.value = true;
+        setTimeout(() => {
+          submitting.value = false;
+          $q.localStorage.set("isAuthenticated", true);
+          router.push("/profile");
+        }, 1000);
+      } else {
+        $q.notify({
+          message: "Неправильные данные!",
+          color: "negative",
+          position: "bottom",
+        });
+      }
+    };
+
+    return {
+      username,
+      password,
+      send,
+      submitting,
+    };
+  },
+});
+</script>
+
+<style></style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <PagePreloader :isLoading="isLoading" />
+  <q-page class="flex column q-px-lg q-pb-lg q-mt-lg">
+    <h4>Пожалуйста, авторизируйтесь!</h4>
+    <LoginForm />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from "vue";
+import PagePreloader from "src/components/PagePreloader.vue";
+import LoginForm from "src/components/LoginPage.vue/LoginForm.vue";
+export default defineComponent({
+  name: "LoginPage",
+  components: {
+    PagePreloader,
+    LoginForm,
+  },
+  setup() {
+    const isLoading = ref(true);
+
+    onMounted(() => {
+      setTimeout(() => {
+        isLoading.value = false;
+      }, 2000);
+    });
+
+    return {
+      isLoading,
+    };
+  },
+});
+</script>
+
+<style></style>
