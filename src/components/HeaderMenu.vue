66c40b46d99bbbeacce88ced656ce604416cac47
Ruslan Shevcov <sasa02sasa@mail.ru>
2023-03-27 14:27:39 +0600

    add icon in header

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,5 +1,8 @@
 <template>
-  <q-toolbar>
+  <q-toolbar class="q-px-md">
+    <q-avatar class="q-mr-xs">
+      <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+    </q-avatar>
     <q-btn flat label="Главная" to="/" />
     <q-btn flat label="Новости" to="/articles" />
     <q-space />
