8a48f1c8fe6b1b3f7adcea903591ec250e5ab1af
Ruslan Shevcov <sasa02sasa@mail.ru>
2023-03-30 17:53:29 +0600

    add mobile menu

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,54 +1,74 @@
 <template>
   <q-toolbar class="q-px-md">
-    <q-avatar class="q-mr-xs">
-      <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-    </q-avatar>
-    <q-btn flat label="Главная" to="/" />
-    <q-btn flat label="Новости" to="/articles" />
-    <q-btn flat label="FAQ" to="/faq" />
-    <q-space />
-    <template v-if="isAuthenticated">
-      <q-btn
-        flat
-        round
-        dense
-        icon="account_circle"
-        class="q-mr-sm"
-        to="/profile"
-      >
-        <q-tooltip> Профиль </q-tooltip>
-      </q-btn>
-      <q-btn flat round dense icon="logout" class="q-mr-sm" @click="logout">
-        <q-tooltip> Выйти </q-tooltip>
-      </q-btn>
-    </template>
-    <template v-else>
-      <q-btn flat round dense icon="login" class="q-mr-sm" to="/login">
-        <q-tooltip> Войти </q-tooltip>
-      </q-btn>
-    </template>
+    <div class="wrapper-computer-menu full-width flex" v-if="$q.screen.gt.xs">
+      <q-avatar class="q-mr-xs">
+        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+      </q-avatar>
+      <q-btn flat label="Главная" to="/" />
+      <q-btn flat label="Новости" to="/articles" />
+      <q-btn flat label="FAQ" to="/faq" />
+      <q-space />
+      <template v-if="isAuthenticated">
+        <q-btn flat round dense icon="account_circle" class="q-mr-sm" to="/profile">
+          <q-tooltip> Профиль </q-tooltip>
+        </q-btn>
+        <q-btn flat round dense icon="logout" class="q-mr-sm" @click="logout">
+          <q-tooltip> Выйти </q-tooltip>
+        </q-btn>
+      </template>
+      <template v-else>
+        <q-btn flat round dense icon="login" class="q-mr-sm" to="/login">
+          <q-tooltip> Войти </q-tooltip>
+        </q-btn>
+      </template>
+    </div>
+    <div v-else-if="$q.screen.lt.sm" class="flex col full-width">
+      <div class="wrapper-above-mobile-menu flex full-width justify-between">
+        <q-avatar class="q-mr-xs">
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
+        <q-btn flat icon="menu" @click="isOpenMobileMenu = !isOpenMobileMenu" />
+      </div>
+      <MobileMenu
+        :isShow="isOpenMobileMenu"
+        :closeMobileMenu="closeMobileMenu"
+        :logout="logout"
+        :isAuthenticated="isAuthenticated"
+      />
+    </div>
   </q-toolbar>
 </template>
 
 <script>
-import { defineComponent, watchEffect, computed } from "vue";
+import { defineComponent, computed, ref } from "vue";
 import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import MobileMenu from "./MobileMenu.vue";
 export default defineComponent({
   name: "HeaderMenu",
+  components: {
+    MobileMenu,
+  },
   setup() {
     const store = useStore();
     const router = useRouter();
     const $q = useQuasar();
+
+    const isOpenMobileMenu = ref(false);
+
     const logout = () => {
       $q.localStorage.set("isAuthenticated", false);
       store.commit("auth/setAuthenticated", false);
+      isOpenMobileMenu.value = false;
       router.push("/");
     };
+
     return {
       isAuthenticated: computed(() => store.getters["auth/getAuthenticated"]),
+      isOpenMobileMenu,
       logout,
+      closeMobileMenu: () => (isOpenMobileMenu.value = false),
     };
   },
 });
---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,96 @@
+<template>
+  <Transition name="opacity" @after-leave="onAnimationEnd">
+    <div class="wrapper-mobile-menu" v-if="isShow">
+      <q-btn flat label="Главная" to="/" />
+      <q-btn flat label="Новости" to="/articles" />
+      <q-btn flat label="FAQ" to="/faq" />
+      <template v-if="isAuthenticated">
+        <q-btn
+          flat
+          round
+          dense
+          icon="account_circle"
+          class="q-mr-sm"
+          to="/profile"
+        >
+          <q-tooltip> Профиль </q-tooltip>
+        </q-btn>
+        <q-btn flat round dense icon="logout" class="q-mr-sm" @click="logout">
+          <q-tooltip> Выйти </q-tooltip>
+        </q-btn>
+      </template>
+      <template v-else>
+        <q-btn flat round dense icon="login" class="q-mr-sm" to="/login">
+          <q-tooltip> Войти </q-tooltip>
+        </q-btn>
+      </template>
+    </div>
+  </Transition>
+</template>
+
+<script>
+import { defineComponent, onMounted, onUpdated } from "vue";
+import { onBeforeRouteLeave } from "vue-router";
+export default defineComponent({
+  name: "MobileMenu",
+  props: {
+    isShow: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    closeMobileMenu: {
+      type: Function,
+      required: true,
+    },
+    isAuthenticated: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props) {
+    const onAnimationEnd = () => {
+      document.documentElement.style.overflow = "auto";
+    };
+
+    onMounted(() => {
+      document.documentElement.style.overflow = "hidden";
+    });
+    onBeforeRouteLeave((to, from) => {
+      props.closeMobileMenu();
+    });
+    return { onAnimationEnd };
+  },
+});
+</script>
+
+<style lang="scss">
+.wrapper-mobile-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: $primary;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 94.5vh;
+  width: 100vw;
+  z-index: 99999;
+  color: #ffffff;
+}
+.opacity-enter-active {
+  animation: opacity-in 0.5s;
+}
+.opacity-leave-active {
+  animation: opacity-in 0.5s reverse;
+}
+@keyframes opacity-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
