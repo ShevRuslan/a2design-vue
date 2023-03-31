bb48e7e72cac71a1bb81f8cf42043f301631346d
Ruslan Shevcov <sasa02sasa@mail.ru>
2023-03-31 23:16:59 +0600

    add plugin Dialog

--- quasar.config.js
+++ quasar.config.js
@@ -97,7 +97,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ["Notify", "LocalStorage"],
+      plugins: ["Notify", "LocalStorage", "Dialog"],
     },
 
     // animations: 'all', // --- includes all animations
